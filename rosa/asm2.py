94ae4e38ce27d030c457ff5dd095a5c29ae24e95
Rosa Zwart <rosa-zwart@hotmail.com>
2021-04-19 23:49:08 +0200

    asm2 experiment try out

---
+++ rosa/asm2.py
@@ -0,0 +1,57 @@
+import diplib as dip
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
+def getstatistics(values):
+    print("Maximum value is " + str(values.max()))
+    print("Minimum value is " + str(values.min()))
+    print("Average is " + str(values.mean()))
+    print("Mode is " + str(stats.mode(values)[0]))
+
+
+def getpixelvalues(img):
+    pixelValues = np.array([])
+
+    for pixelLocation in range(len(img)):
+        listElement = img[pixelLocation]
+        pixelValues = np.append(pixelValues, int(listElement[0]))
+
+    return pixelValues
+
+
+def processimage():
+    # Read image
+    rect = dip.ImageReadTIFF("C:/Users/rosa-/PycharmProjects/iaim_own_work/images_assignment2/rect1.tif")
+
+    # Reduce noise filter 1
+    #rect = dip.MedianFilter(rect, "rectangular")
+    # Reduce noise filter 2
+    #rect = dip.Gauss(rect, 4)
+
+    #Get threshold
+    _, thresh = dip.Threshold(rect)
+    #print(thresh)
+
+    rectbin = rect < thresh
+
+    #showimage(rectbin)
+
+    #values = getpixelvalues(rect)
+    #getstatistics(values)
+
+    rectbin = dip.Label(rectbin)
+    showimage(rectbin)
+    m = dip.MeasurementTool.Measure(rectbin, rect, ['Size', 'Perimeter'])
+    print(m)
+
+
+def showimage(img):
+    dip.PyDIPviewer.Show(img)
+    #img.Show()
+    input()
+    return 0
+
+
+processimage()
