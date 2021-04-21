db10dbb0d1d235f1e8978c9533c4c2c4fe25d3fe
Rosa Zwart <rosa-zwart@hotmail.com>
2021-04-22 00:35:43 +0200

    code that acquires information that has been stated in the assignment answer document

--- rosa/asm2.py
+++ rosa/asm2.py
@@ -1,53 +1,37 @@
 # contributions of all team members used and combined:
 
-from datetime import datetime
-import time
+import diplib
+import numpy as np
 
 import diplib as dip
-from diplib import PyDIPjavaio
-
-import PIL
 from PIL import Image as PilImage
-
-import numpy as np
+import PIL
+from diplib import PyDIPjavaio
 
 import matplotlib.pyplot as plt
 
-# ---- Common Util ----
-
-def generate_date_time_str():
-    date_time_str = str(datetime.now().strftime("%Y-%m-%d_%H%M%S"))
-    return date_time_str
+from util import UtilFunctions
 
 
-def save_image_to_default_project_folder(img, image_name):
-    dip.ImageWriteTIFF(img, image_name)
-
-
-def sleep(secs: int):
-    time.sleep(secs)
-
-def get_mean_std(array):
-    mean = np.mean(array)
-    std = np.std(array)
-
-    return mean, std
-
-
-# ---- Image Util ----
 
 def measure_size_and_perimeter(img):
-    threshold_value = threshold(img)
+    threshold_value = UtilFunctions.threshold(img)
     # Segment image
     segm_img = img < threshold_value
+
+    # Opening (erosion for removing smaller objects then dilation for restoring remaining objects)
+    #segm_img = dip.BinaryOpening(segm_img, -1, 4)
+    #segm_img = dip.BinaryDilation(segm_img)
+
+    UtilFunctions.show_image_in_dip_view(segm_img, 4)
+
     # Label segmented objects
     segm_img = dip.Label(segm_img)
-
-    #show_image_in_dip_view(segm_img, 1)
 
     # Get measurements
     px_measurements = dip.MeasurementTool.Measure(segm_img, img, ['Size', 'Perimeter'])
     print(px_measurements)
+
     # Get array with all sizes
     sizes = np.array(px_measurements['Size']).transpose()
     perimeters = np.array(px_measurements['Perimeter']).transpose()
@@ -70,29 +54,18 @@ def plot_relative_discretization_error(mean_array, std_array, label, img_series)
     fig.savefig("../plot_output/" + label + "_" + img_series)
 
 
-def show_image_in_dip_view(img, sec):
-    dip.PyDIPviewer.Show(img)
-    time.sleep(sleep_sec)
-
-
-def threshold(img: PyDIPjavaio.ImageRead):
-    _, threshold = dip.Threshold(img)
-    return threshold
-
-
-def obtain_image(img_name):
-    image_file_path = "../image_files/" + image_name
-    img = dip.ImageReadTIFF(image_file_path)
-
-    return img
-
-
 if __name__ == '__main__':
     sleep_sec: int = 3
 
     # Initialize and state image name lists
-    image_name_list: list = ["rect1", "rect2", "rect3", "rect4"]
-    image_name_list_noise: list = ["rect1a", "rect1b", "rect2a", "rect2b", "rect3a", "rect3b", "rect4a", "rect4b"]
+    image_name_list: list = []
+    image_name_list_noise: list = ["rect4b"]
+
+    # Initialize parameters for Gaussian filter
+    gauss_parameter_list = [3]
+
+    # Initialize parameters for Median filter and then repeat steps
+    median_parameter_list = ['elliptic']
 
     # Initialize statistic arrays
     mean_size = []
@@ -108,51 +81,87 @@ if __name__ == '__main__':
         sizes, perimeters = measure_size_and_perimeter(curr_img)
 
         # Get statistics
-        mean, std = get_mean_std(sizes)
+        mean, std = UtilFunctions.get_mean_std(sizes)
         mean_size.append(mean)
         std_size.append(std)
         print("Size:  | mean    ", mean, " | std    ", std, "  |")
 
-        mean, std = get_mean_std(perimeters)
+        mean, std = UtilFunctions.get_mean_std(perimeters)
         mean_perimeter.append(mean)
         std_perimeter.append(std)
         print("Perimeter:  | mean    ", mean, "  | std    ", std, "  |")
 
     # Now, all the mean and std values are known for images (rect)1-4 that are used for the following:
     # Plot the graph of the relative discretization error of the size
-    plot_relative_discretization_error(np.array(mean_size), np.array(std_size), 'size', 'no_noise')
+    #plot_relative_discretization_error(np.array(mean_size), np.array(std_size), 'size', 'no_noise')
     # Plot the graph of the relative discretization error of the perimeter
-    plot_relative_discretization_error(np.array(mean_perimeter), np.array(std_perimeter), 'perimeter', 'no_noise')
+    #plot_relative_discretization_error(np.array(mean_perimeter), np.array(std_perimeter), 'perimeter', 'no_noise')
+
+    # Initialize date and time
+    date_time_str = UtilFunctions.generate_date_time_str()
 
     '''
-    # Initialize parameters for Gaussian filter
-    gauss_parameter_list = [2, 5]
-    # Initialize parameters for Median filter
-    median_parameter_list = ['rectangular', 'elliptic']
-
-    # Initialize data and time
-    date_time_str = generate_date_time_str()
-
     for image_name in image_name_list_noise:
         # Load current image
-        curr_img = obtain_image(image_name)
+        curr_img = UtilFunctions.obtain_image(image_name)
         # Apply Gaussian filter with different parameters
         for gauss_parameter_value in gauss_parameter_list:
             curr_gauss_img = dip.Gauss(curr_img, gauss_parameter_value)
             file_name = date_time_str + image_name + "_gauss_" + str(gauss_parameter_value)
-            # Does not work:
-            save_image_to_default_project_folder(curr_gauss_img, file_name)
+            print(file_name)
+            UtilFunctions.show_image_in_dip_view(curr_gauss_img, sleep_sec)
             # Here, also measure size and perimeter per image and per Gaussian filter with certain filter (for part 2.3, question 7)
+            sizes, perimeters = measure_size_and_perimeter(curr_gauss_img)
+
+            # Filters all irrelevant objects from incorrect segmentations
+            UtilFunctions.print_largest_measurements(sizes[0], perimeters[0])
+
             # Then, get the statistics (mean and std) from these measurements
-        
+            mean, std = UtilFunctions.get_mean_std(sizes)
+            mean_size.append(mean)
+            std_size.append(std)
+            print("Size:  | mean    ", mean, " | std    ", std, "  |")
+
+            mean, std = UtilFunctions.get_mean_std(perimeters)
+            mean_perimeter.append(mean)
+            std_perimeter.append(std)
+            print("Perimeter:  | mean    ", mean, "  | std    ", std, "  |")
+
+    '''
+
+    for image_name in image_name_list_noise:
+        # Load current image
+        curr_img = UtilFunctions.obtain_image(image_name)
+        # Apply Gaussian filter with different parameters
+        for median_parameter in median_parameter_list:
+            curr_median_img = dip.MedianFilter(curr_img, median_parameter)
+            file_name = date_time_str + image_name + "_median_" + str(median_parameter)
+            print(file_name)
+            UtilFunctions.show_image_in_dip_view(curr_median_img, sleep_sec)
+            # Here, also measure size and perimeter per image and per Gaussian filter with certain filter (for part 2.3, question 7)
+            sizes, perimeters = measure_size_and_perimeter(curr_median_img)
+
+            # Filters all irrelevant objects from incorrect segmentations
+            UtilFunctions.print_largest_measurements(sizes[0], perimeters[0])
+
+            # Then, get the statistics (mean and std) from these measurements
+            mean, std = UtilFunctions.get_mean_std(sizes)
+            mean_size.append(mean)
+            std_size.append(std)
+            print("Size:  | mean    ", mean, " | std    ", std, "  |")
+
+            mean, std = UtilFunctions.get_mean_std(perimeters)
+            mean_perimeter.append(mean)
+            std_perimeter.append(std)
+            print("Perimeter:  | mean    ", mean, "  | std    ", std, "  |")
+
+
     # Now, all the mean and std values are known for images (rect)1a,1b-4a,4b that are used for the following:
     # Plot the relative discretization error graphs for size and perimeter where FOR EACH label (size and perimeter):
     # FIRST graph shows values from images 1a-4a with first Gaussian filter, 
     # where the SECOND graph shows values from images 1a-4a with second Gaussian filter, where
     # THIRD graph shows values from images 1b-4b with first Gaussian filter and where FOURTH graph shows values from
     # images 1b-4b with second Gaussian filter
-    
-    # Do line 136 - 153 also for Median filter
     
     # SIGNAL TO NOISE RATIO
     # -> Characterizes image quality. mean_sig/std_sig where mean_sig is average signal value and std_sig is std of the signal
@@ -167,5 +176,4 @@ if __name__ == '__main__':
         # Third series: rect1b, rect2b, rect3b, rect4b
         # Put the results in a table
     
-    # Lastly, produce plot (with SNR against sum of all measurements in that image) PER image     
-    '''
+    # Lastly, produce plot (with SNR against sum of all measurements in that image) PER image
