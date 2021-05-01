328e2fe2db1fb370f3536607b47012d42aef00c4
Adéla Šterberová <asterberova@gmail.com>
2021-05-01 11:02:06 +0200

    q8-q10 for 3.3 part

---
+++ all_asm/asm3/q8_Mathematical_morphology.py
@@ -0,0 +1,35 @@
+import diplib as dip
+import numpy as np
+import matplotlib.pyplot as plt
+
+from util.common_util import CommonUtil
+from util.image_util import ImageUtil
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    image_name = 'scale-img'
+
+    input_dir_str: str = CommonUtil.obtain_project_default_input_dir_path() + "asm3/"
+    output_dir_str: str = CommonUtil.obtain_project_default_output_dir_path() + "q8/"
+    CommonUtil.create_missing_dir(output_dir_str)
+
+    se_shape = ['elliptic', 'diamond', 'rectangular']
+    se_size = [5, 11, 21, 31, 51]
+
+    for se in se_shape:
+        for size in se_size:
+            print("SE:", se, "size:", size)
+
+            original_image = ImageUtil.obtain_image(image_name, input_dir_str)
+
+            black_hat_image = ImageUtil.black_hat(original_image, size, se)
+
+            file_name = image_name + "_black_hat_" + se + "_" + str(size) + ".tif"
+            ImageUtil.show_image_in_dip_view(black_hat_image, 10, file_name)
+
+            # CommonUtil.save_image_to_folder(black_hat_image, output_dir_str, file_name)
