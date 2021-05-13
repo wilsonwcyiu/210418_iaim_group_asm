2ae211b9bddc80dbbec6c5be42399cb131b44739
Wilson Yiu <wilson.yiu@mail.digiec.com>
2021-05-13 22:18:17 +0200

    update

--- all_asm/asm4/model/cell.py
+++ all_asm/asm4/model/cell.py
@@ -1,11 +1,11 @@
 class Cell():
 
 
-    def __init__(self, cell_id, cell_display_name = None):
+    def __init__(self, cell_id, cell_display_name = None, cell_xy_coord_tuple: tuple = None, perimeter: float = None, area: float = None):
         self.cell_id: int = cell_id
         self.cell_display_name: str = None
 
-        self.cell_xy_coord_tuple: tuple = None
+        self.cell_xy_coord_tuple: tuple = cell_xy_coord_tuple
 
-        self.perimeter: float = None
-        self.area: float = None
+        self.perimeter: float = perimeter
+        self.area: float = area
