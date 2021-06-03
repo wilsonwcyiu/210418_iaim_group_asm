14e08411cb57aea8db62dd294d3856efa28aafdf
Wilson Yiu <wilson.yiu@mail.digiec.com>
2021-06-03 11:52:53 +0200

    max projection

--- all_asm/asm6/q4_try2.py
+++ all_asm/asm6/q4_try2.py
@@ -15,13 +15,10 @@ if __name__ == '__main__':
     input_dir: str = CommonUtil.obtain_project_default_input_dir_path() + 'asm6/'
     proj_output_dir_path: str = CommonUtil.obtain_project_default_output_dir_path() + CommonUtil.generate_date_time_str() + "/"
 
-
-
-
     image_name_list: list = ["CHROMO3D.ics"]
 
     for image_name in image_name_list:
-        curr_img: diplib.PyDIP_bin.Image = ImageUtil.obtain_diplib_image(image_name, input_dir);        ImageUtil.show_image_in_dip_view(curr_img)
+        curr_img: diplib.PyDIP_bin.Image = ImageUtil.obtain_diplib_image(image_name, input_dir);        ImageUtil.show_image_in_dip_view(curr_img, title="original image")
 
 
         image_width: int = 140
@@ -33,18 +30,33 @@ if __name__ == '__main__':
 
         # print(type(pixel_3d), pixel_3d.shape)
 
-        high_intensity_list = np.zeros((image_width, image_height))
-
-
+        max_projection_list = np.zeros((image_width, image_height))
         for layer_idx in range(16):
             for width_idx in range(image_width):
                 for height_idx in range(image_height):
                     pixel_value = pixel_3d[layer_idx][width_idx][height_idx]
 
-                    if pixel_value > high_intensity_list[width_idx][height_idx]:
-                        high_intensity_list[width_idx][height_idx] = pixel_value
+                    if pixel_value > max_projection_list[width_idx][height_idx]:
+                        max_projection_list[width_idx][height_idx] = pixel_value
 
-        ImageUtil.show_image_in_dip_view(high_intensity_list)
+        ImageUtil.show_image_in_dip_view(max_projection_list, title="Max projection from original image")
+
+
+
+        threshold_img = ImageUtil.segment_image_white(curr_img)
+        pixel_list = ImageUtil.obtain_pixel_value_list(threshold_img)
+        pixel_3d = np.reshape(pixel_list, (image_layers, image_width, image_height));
+
+        max_projection_list = np.zeros((image_width, image_height))
+        for layer_idx in range(16):
+            for width_idx in range(image_width):
+                for height_idx in range(image_height):
+                    pixel_value = pixel_3d[layer_idx][width_idx][height_idx]
+
+                    if pixel_value > max_projection_list[width_idx][height_idx]:
+                        max_projection_list[width_idx][height_idx] = pixel_value
+
+        ImageUtil.show_image_in_dip_view(max_projection_list, title="Max projection from binary image")
 
 
 
